dcb5fa749a720b8032aa1d333a905a48fe7efb32
arunpk95 <arunpk95@gmail.com>
2022-01-27 05:23:43 +0000

    Task 2.3

--- public-api/app/Http/Controllers/UserFoodController.php
+++ public-api/app/Http/Controllers/UserFoodController.php
@@ -5,11 +5,15 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
+use Response;
 
 class UserFoodController extends Controller
 {
     private $privateApiUrl;
+    private static $errStatusCode=500;
+    private static $errMessage=['message' => "Sorry, some error occurred."];
 
+    
     public function __construct()
     {
         $this->privateApiUrl = env('PRIVATE_API_URL');
@@ -18,19 +22,34 @@ class UserFoodController extends Controller
     public function foods(int $userId)
     {
         $response = Http::get("{$this->privateApiUrl}/users/{$userId}/foods");
-        return $response->json();
+        if($response->successful()){
+            return $response->json();
+        }
+        else{
+            return Response::json(static::$errMessage,static::$errStatusCode);
+        }
     }
 
     public function food(int $userId, int $foodId)
     {
         $response = Http::get("{$this->privateApiUrl}/users/{$userId}/foods/{$foodId}");
-        return $response->json();
+        if($response->successful()){
+            return $response->json();
+        }
+        else{
+            return Response::json(static::$errMessage,static::$errStatusCode);
+        }
     }
 
     public function deleteFood(int $userId, int $foodId)
     {
         $response = Http::delete("{$this->privateApiUrl}/users/{$userId}/foods/{$foodId}");
-        return $response->json();
+        if($response->successful()){
+            return $response->json();
+        }
+        else{
+            return Response::json(static::$errMessage,static::$errStatusCode);
+        }
     }
 
     public function addFood(Request $request, int $userId, int $foodId)
@@ -40,6 +59,11 @@ class UserFoodController extends Controller
         $response = Http::put("{$this->privateApiUrl}/users/{$userId}/foods/{$foodId}", [
             'servingsPerWeek' => $servingsPerWeek
         ]);
-        return $response->json();
+        if($response->successful()){
+            return $response->json();
+        }
+        else{
+            return Response::json(static::$errMessage,static::$errStatusCode);
+        }
     }
 }
