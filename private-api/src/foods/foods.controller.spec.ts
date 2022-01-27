300edd16bee2ab779c4a9994882c37ead1c78e5d
arunpk95 <arunpk95@gmail.com>
2022-01-27 07:16:50 +0000

    Task 2.2

--- private-api/src/foods/foods.controller.spec.ts
+++ private-api/src/foods/foods.controller.spec.ts
@@ -1,32 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateFoodDto } from './dto/create-food.dto';
 import { Food } from './entities/food.entity';
 import { FoodsController } from './foods.controller';
 import { FoodsService } from './foods.service';
 
-const mockRepository = jest.fn(() => ({
-  metadata: {
-    columns: [],
-    relations: [],
-  },
-}));
-
 describe('FoodsController', () => {
-  let controller: FoodsController;
+  let foodController: FoodsController;
+  let foodService: FoodsService
 
   beforeEach(async () => {
+    const FoodServiceProvider = { provide: FoodsService, 
+
+      useFactory:() => ({
+        create: jest.fn(()=>[{}]),
+        findAll: jest.fn(()=>{return [{"id":167765,"description":"Watermelon, raw","publicationDate":"2019-04-01T00:00:00.000Z"}]}),
+        findOne: jest.fn(()=>{return {}}),
+        update: jest.fn(()=>{return {}}),
+        remove: jest.fn(()=>{}),
+      }) 
+    }
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FoodsController],
       providers: [
         FoodsService,
-        { provide: getRepositoryToken(Food), useClass: mockRepository },
+        FoodServiceProvider,
       ],
     }).compile();
 
-    controller = module.get<FoodsController>(FoodsController);
+    foodController = module.get<FoodsController>(FoodsController);
+    foodService = module.get<FoodsService>(FoodsService)
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('calling create food method', () => {
+    const dto = new CreateFoodDto();
+    expect(foodController.create(dto)).not.toEqual(null);
   });
+  
+  
+  it('testing find by keyword', () => {
+    expect(foodController.findAll("WaterMelon, raw",1,10)).toEqual([{"id":167765,"description":"Watermelon, raw","publicationDate":"2019-04-01T00:00:00.000Z"}]);
+  });
+
+  it('testing find by id', () => {
+    expect(typeof foodController.findOne(10)).toEqual("object");
+  });
+
+  it('testing update', () => {
+    const dto = new CreateFoodDto();
+    
+    expect(typeof foodController.update(11,dto)).toEqual("object");
+  });
+
+
+  it('testing delete by id', () => {
+    expect(typeof foodController.remove(10)).toEqual("object");
+  });
+
 });
